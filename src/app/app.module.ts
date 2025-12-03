cd8f682473877a07627ca2e5c36805412c21e54c
AlfredoRiveraaa <alfrexri550@gmail.com>
2025-12-02 19:22:07 -0600

    Implementación de formulario Admin y Maestro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+//Este import es para los servicios HTTP
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,8 +14,12 @@ import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-scr
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
-import { NavbarComponent } from './partials/navbar/navbar.component';
+import {NavbarComponent} from './partials/navbar/navbar.component';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 import { RegistroAdminComponent } from './partials/registro-admin/registro-admin.component';
 import { RegistroAlumnosComponent } from './partials/registro-alumnos/registro-alumnos.component';
 import { RegistroMaestrosComponent } from './partials/registro-maestros/registro-maestros.component';
@@ -35,7 +42,11 @@ import { RegistroMaestrosComponent } from './partials/registro-maestros/registro
     MatIconModule,
     MatDividerModule,
     FormsModule,
-    MatRadioModule
+    MatRadioModule,
+    MatInputModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
